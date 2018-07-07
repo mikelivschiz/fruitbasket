8282f7a328853d3fb264a924c01fa9efa4c44a8f
mikelivschiz <mikelivschiz@gmail.com>
2018-07-07 16:28:46 -0400

    added recursion and functions

--- fruit_basket.py
+++ fruit_basket.py
@@ -1,10 +1,23 @@
 #!/usr/bin/python
-def fruit_basket():
-	basket_list = ["apple", "pear", "banana", "orange", "grape"]
-	fruit = raw_input("Guess a fruit...\n")
-	if (fruit) in basket_list:
-		print ("Good guess!")
-	else:
-		print ("Nope")
 
-fruit_basket()
+def fruit_basket(basket_list):
+	myvar1 = 0
+#	basket_list = ["jackfruit", "durian", "grapefruit", "pineapple", "banana"]
+	while myvar1 < 5:
+		fruit = raw_input("Guess a fruit...\n")
+		if (fruit) in basket_list:
+			print ("Good guess!")
+			break
+		else:
+			myvar1 += 1
+			if myvar1 == 5:
+				print("You suck.  You lose.")
+			else:
+				print("That one is not in the basket.")
+#fruit_basket()
+
+def main():
+	basket_list = ["jackfruit", "durian", "grapefruit", "pineapple", "banana"]
+	fruit_basket(basket_list)
+
+main()
